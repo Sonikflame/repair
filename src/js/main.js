5ae4b763094739073acdd14e760d23d0154a06c3
Sonikflame <sonikflame@mail.ru>
2020-01-03 19:35:14 +0200

    Адаптация второго экрана

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
-   $('.fullpage').fullpage({
-    scrollOverflow: true,
-   });
- });
+    $('.fullpage').fullpage({
+      scrollOverflow: true,
+      scrollOverflowOptions: {click: false},
+      afterRender: function () {
+        $('.projects__last').html($('.projects__slides').length);
+      },
+      afterSlideLoad: function (section, origin, destination) {
+        $('.projects__first').html(++destination.index);
+      }
+    });
+
+    $(document).on('click', '.projects__mini a', changeImg);
+
+    function changeImg(event) {
+      event.preventDefault();
+      $('.projects__big img').attr('src', $(this).attr('data-src'));
+    }
+});
